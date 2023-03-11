07c0f7a1d80c529cf9b74c351ac4f4f1a0c3b392
e012052020alice <119144400+e012052020alice@users.noreply.github.com>
2023-03-11 12:08:20 +0800

    Update 控制系統

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChimmyBear
+{
+    public class ControlSystem : MonoBehaviour
+    {
+        [SerializeField, Header("目標物件")]
+        private Transform target;
+
+        private void Update()
+        {
+            LookTarget();
+        }
+        /// <summary>
+        /// 面向目標
+        /// </summary>
+        private void LookTarget()
+        {
+            //變形元件.面向(目標物件)
+            transform.LookAt(target);
+            //x與z歸零
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+        }
+    }
+    
+
+}
+
